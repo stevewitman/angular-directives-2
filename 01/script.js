d4420dfde94c8ba7e6a3237872550050a1f5965b
Steve Witman <stevewitman@gmail.com>
2015-07-15 23:40:39 -0600

    add simple user-info-directive

--- 01/script.js
+++ 01/script.js
@@ -3,3 +3,10 @@ angular.module('app', []);
 angular.module('app').controller('mainCtrl', function($scope) {
 
 });
+
+angular.module('app').directive('userInfoCard', function() {
+  return {
+    template: 'User Info Card',
+    restrict: 'E'
+  }
+})
