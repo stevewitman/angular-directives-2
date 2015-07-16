3182517dfddefff07cb2c3017408932d5e3be5cf
Steve Witman <stevewitman@gmail.com>
2015-07-15 23:51:30 -0600

    bind user data to user-info-card

--- 01/script.js
+++ 01/script.js
@@ -1,12 +1,19 @@
 angular.module('app', []);
 
 angular.module('app').controller('mainCtrl', function($scope) {
-
+  $scope.user = {
+    name: 'David Robinson',
+    address: {
+      street: '123 Main St',
+      city: 'Springfield',
+      planet: 'pluto'
+    }
+  }
 });
 
 angular.module('app').directive('userInfoCard', function() {
   return {
-    template: 'User Info Card',
+    template: 'Name: {{ user.name }}<br>Address: {{ user.address.street }}<br> {{ user.address.city }}<br> {{ user.address.planet }}',
     restrict: 'E'
   }
 })
